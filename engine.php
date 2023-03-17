5cc8edfac7fc9cffa8534b3e26df92a31f28fb78
kunfirekun <kunfirekun@gmail.com>
2023-03-17 15:10:36 +0300

    edited ip address function and comments

--- engine.php
+++ engine.php
@@ -1,5 +1,29 @@
 <?php 
+require_once "config.php";
+//function to encrypt email
+function getEncryptedUserEmail(){
 
+    $email = $con->real_escape_string($_POST['email']);
+    //convert email to lower case
+    $email_lower=strtolower($email);
+    //sanitization to remove illegal characters
+    $email_sanitize = filter_var($email_lower, FILTER_SANITIZE_EMAIL);
+    // Validate email
+    $email_validated=filter_var($email_sanitize, FILTER_VALIDATE_EMAIL);
+
+    $email=$email_validated; // validated and sanitized email
+    $privateKey1 	= 'NOWEINF32523EFW63HGBERV34235'; // user defined key1
+    $secretKey1		= 'hd203dh2bx2zp'; // user defined secret key1
+    $encryptMethod1      = "AES-256-CBC";
+    $string1 		=$email ; // user defined value2
+
+    $key1 = hash('sha256', $privateKey1);
+    $ivalue1 = substr(hash('sha256', $secretKey1), 0, 16); // sha256 is hash_hmac_algo
+    $result1 = openssl_encrypt($string1, $encryptMethod1, $key1, 0, $ivalue1);
+    $encrypted_email= base64_encode($result1);  // encrypted email output
+
+    return $encrypted_email;
+}
 
 //function to generate random discount code
 function getDiscountCode(){
@@ -28,17 +52,18 @@ function getEncryptedUserIpAddr(){
     }
     
 
-    $ip_address= $ip ;// user address
-    $privateKey1 	= '8NOJQCIPN9-runD-HFNFIQJF'; // user defined key1
-    $secretKey1		= '4NCaasjfj-0E2U9Edi2E'; // user defined secret key1
-    $encryptMethod1      = "AES-256-CBC";
-    $string1 		=$ip_address ; // user defined value2
+    $ip_address=$ip ;//get ip value
+    $privatekey_ip	= '8NOJQCIPN9-runD-HFNFIQJF'; // user defined key
+    $secretkey_ip		= '4NCaasjfj-0E2U9Edi2E'; // user defined secret key
+    $encryptMethod_ip      = "AES-256-CBC";
 
-    $key1 = hash('sha256', $privateKey1);
-    $ivalue1 = substr(hash('sha256', $secretKey1), 0, 16); // sha256 is hash_hmac_algo
-    $result1 = openssl_encrypt($string1, $encryptMethod1, $key1, 0, $ivalue1);
-    $user_address= base64_encode($result1);  // output1 is a encripted value of the user input under email_encode
+    $string_ip 		=$ip_address ; // ip address value
 
-    return $user_address;
+    $key_ip = hash('sha256', $privatekey_ip);
+    $ivalue_ip = substr(hash('sha256', $secretkey_ip), 0, 16); // sha256 is hash_hmac_algo
+    $result_ip = openssl_encrypt($string_ip, $encryptMethod_ip, $key_ip, 0, $ivalue_ip);
+    $encrypted_ip= base64_encode($result_ip);  // output encrypted ip
+
+    return $encrypted_ip;
 
 }
